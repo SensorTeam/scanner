1433ad38abade0e7b50c42b12ed0fc66134aadc2
CJ Melegrito <cj@mlgrto.com>
2018-07-19 16:33:58 +1000

    Interval scanning

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const app = require('express')()
 const http = require('http').Server(app)
 const io = require('socket.io')(http)
 
+const im = require('image-data-uri')
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html')
 })
@@ -10,11 +12,20 @@ app.get('/w', (req, res) => {
   res.sendFile(__dirname + '/webcam.min.js')
 })
 
+app.get('/j', (req, res) => {
+  res.sendFile(__dirname + '/jquery-1.11.1.js')
+})
+
 io.on('connection', (socket) => {
+  let counter = 0
+
   console.log('CONNECTED')
 
   socket.on('transmit', (data) => {
-    console.log('DATA: ' + data)
+    const path = __dirname + '/data/file' + counter.toString() + '.jpg'
+    counter++
+    im.outputFile(data, path)
+    console.log('RECEIVED: ' + path)
   })
 
   socket.on('disconnect', () => {
